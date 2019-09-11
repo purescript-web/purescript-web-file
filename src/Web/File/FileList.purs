2bbef76f3dcf62c751ab7b22e9fa78375417b82d
William Rusnack <williamrusnack@gmail.com>
2019-09-11 13:10:49 -0400

    items :: FileList -> Array File

--- src/Web/File/FileList.purs
+++ src/Web/File/FileList.purs
@@ -2,12 +2,15 @@ module Web.File.FileList
   ( FileList
   , length
   , item
+  , items
   ) where
 
 import Prelude
 
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Nullable (Nullable, toMaybe)
+import Data.Array (snoc)
+import Data.Tuple (Tuple(..))
 import Web.File.File (File)
 
 foreign import data FileList :: Type
@@ -20,3 +23,12 @@ foreign import _item :: Int -> FileList -> Nullable File
 -- | Get `File` at the certain position
 item :: Int -> FileList -> Maybe File
 item i = toMaybe <<< _item i
+
+items :: FileList -> Array File
+items fl = untilNothing (\c fs -> Tuple (c + 1) <<< snoc fs <$> item c fl) 0 mempty
+
+untilNothing :: forall a b. (a -> b -> Maybe (Tuple a b)) -> a -> b -> b
+untilNothing f x y = let m = f x y in
+  case m of (Just (Tuple x' y')) -> untilNothing f x' y'
+            Nothing -> y
+

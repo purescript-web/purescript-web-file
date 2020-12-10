5d177806cd9e2821d39217c3e7c42a9467089906
Thomas Honeyman <admin@thomashoneyman.com>
2020-12-10 10:53:03 -0800

    Lint FileReader.js

--- src/Web/File/FileReader.js
+++ src/Web/File/FileReader.js
@@ -1,15 +1,32 @@
-/* global FileReader */
 "use strict";
 
-exports.fileReader = function () { return new FileReader(); };
+exports.fileReader = function () {
+  return new FileReader();
+};
 
-exports.error = function (fr) { return function () { return fr.error; }; };
+exports.error = function (fr) {
+  return function () {
+    return fr.error;
+  };
+};
 
-exports.readyStateImpl = function (fr) { return function () { return fr.readyState; }; };
+exports.readyStateImpl = function (fr) {
+  return function () {
+    return fr.readyState;
+  };
+};
 
-exports.result = function (fr) { return function () { return fr.result; }; };
+exports.result = function (fr) {
+  return function () {
+    return fr.result;
+  };
+};
 
-exports.abort = function (fr) { return function () { fr.abort(); }; };
+exports.abort = function (fr) {
+  return function () {
+    fr.abort();
+  };
+};
 
 exports.readAsText = function (blob) {
   return function (fr) {
